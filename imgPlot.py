5784705b1cb59c7f4c1e45061601a1cf765ec55f
Jianming Sang <jmsang@gatech.edu>
2016-08-25 16:26:57 -0400

    added the data reduce functionality

--- imgPlot.py
+++ imgPlot.py
@@ -11,14 +11,32 @@ class ImagePlot(ImageBase):
 	data_dir = None
 	image_data = None
 	image_count = 0
+	dir_name = 'reduced_data'
 
 	def __init__(self, *args, **kwargs):
 		super(ImagePlot, self).__init__(*args, **kwargs)
-		self.image_plot()
+		self.reduce_data()
 
 	def get_things_ready(self):
+		self.create_output_data_dir()
 		if not self.data_dir:
 			self.data_dir = self.image_dir
+
+	def reduce_data(self):
+		for file_path, file_name in self.get_image_data_files():
+			print file_name
+			data = self.read_image_data(file_path)
+			reduced_data = pd.DataFrame(columns=data.columns)
+			lower = higher = 0
+			for i in xrange(1, self.diameter/2+1):
+				higher = np.power(self.MICRO_TO_PIXEL*i, 2)
+				ring_data = data.loc[lower:higher-1]
+				reduced_data.loc[i] = ring_data.mean()
+				lower = higher
+			self.save_data(reduced_data, file_name)
+
+	def save_data(self, data, file_name):
+		data.to_csv(self.output_data_dir+'/'+file_name)
 
 	def image_plot(self):
 		if self.image_data is None:
@@ -38,8 +56,9 @@ class ImagePlot(ImageBase):
 		return self.image_data
 
 	def get_image_data_files(self):
-		print self.data_dir
 		for root, directories, files in os.walk(self.data_dir):
+			if root == self.output_data_dir:
+				continue
 			for file_name in files:
 				if file_name.endswith('.csv'):
 					file_path = os.path.join(root, file_name)
